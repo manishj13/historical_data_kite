407e65e8aff58178ea0194fb32b38580deff1e12
manishj13 <83181822+manishj13@users.noreply.github.com>
2021-06-10 11:58:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import datetime as dt
 import pandas as pd
 import streamlit as st
 import base64
-from io import BytesIO
+import json
+import pickle
+import uuid
+import re
+
 
 #Get input from user
 global user_id,token,timeframe, ticker
@@ -119,6 +123,113 @@ def get_table_download_link(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
     return f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+
+#download button
+def download_button(object_to_download, download_filename, button_text, pickle_it=False):
+    """
+    Generates a link to download the given object_to_download.
+
+    Params:
+    ------
+    object_to_download:  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv,
+    some_txt_output.txt download_link_text (str): Text to display for download
+    link.
+    button_text (str): Text to display on download button (e.g. 'click here to download file')
+    pickle_it (bool): If True, pickle file.
+
+    Returns:
+    -------
+    (str): the anchor tag to download object_to_download
+
+    Examples:
+    --------
+    download_link(your_df, 'YOUR_DF.csv', 'Click to download data!')
+    download_link(your_str, 'YOUR_STRING.txt', 'Click to download text!')
+
+    """
+    if pickle_it:
+        try:
+            object_to_download = pickle.dumps(object_to_download)
+        except pickle.PicklingError as e:
+            st.write(e)
+            return None
+    
+    else:
+        if isinstance(object_to_download, bytes):
+            pass
+        
+        elif isinstance(object_to_download, pd.DataFrame):
+            object_to_download = object_to_download.to_csv(index=False)
+        
+        # Try JSON encode for everything else
+        else:
+            object_to_download = json.dumps(object_to_download)
+    
+    try:
+        # some strings <-> bytes conversions necessary here
+        b64 = base64.b64encode(object_to_download.encode()).decode()
+    
+    except AttributeError as e:
+        b64 = base64.b64encode(object_to_download).decode()
+    
+    button_uuid = str(uuid.uuid4()).replace('-', '')
+    button_id = re.sub('\d+', '', button_uuid)
+
+    prim_color = st.config.get_option('theme.primaryColor') or '#F43365'
+    bg_color = st.config.get_option('theme.backgroundColor') or '#000000'
+    sbg_color = st.config.get_option('theme.secondaryBackgroundColor') or '#f1f3f6'
+    txt_color = st.config.get_option('theme.textColor') or '#000000' 
+    font = st.config.get_option('theme.font') or 'sans serif'  
+
+
+    custom_css = f"""
+        <style>
+            #{button_id} {{
+                background-color: {bg_color};
+                color: {txt_color};
+                padding: 0.25rem 0.75rem;
+                position: relative;
+                line-height: 1.6;
+                border-radius: 0.25rem;
+                border-width: 1px;
+                border-style: solid;
+                border-color: {bg_color};
+                border-image: initial;
+                filter: brightness(105%);
+                justify-content: center;
+                margin: 0px;
+                width: auto;
+                appearance: button;
+                display: inline-flex;
+                family-font: {font};
+                font-weight: 400;
+                letter-spacing: normal;
+                word-spacing: normal;
+                text-align: center;
+                text-rendering: auto;
+                text-transform: none;
+                text-indent: 0px;
+                text-shadow: none;
+                text-decoration: none;
+            }}
+            #{button_id}:hover {{
+                
+                border-color: {prim_color};
+                color: {prim_color};
+            }}
+            #{button_id}:active {{
+                box-shadow: none;
+                background-color: {prim_color};
+                color: {sbg_color};
+                }}
+        </style> """
+    
+    dl_link = custom_css + f'<a download="{download_filename}" class= "" id="{button_id}" ' \
+                           f'href="data:file/txt;base64,{b64}">{button_text}</a><br></br>'
+    
+    return dl_link
+
 
 #For a list of tickers
 
